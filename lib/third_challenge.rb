07fd736707fef225475ca73fd5b1418324f7d122
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-19 17:11:26 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,43 +1,44 @@
-def second_challenge
+def third_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+   :montague => {
       :patriarch => {
+        #your key/value pairs here        
         :name => "Lord Montague",
         :age => "53"
-      },
+        },
       :matriarch => {
+        #your key/value pairs here
         :name => "Lady Montague",
         :age => "54"
-      },
+        },
       :hero => {
+        #your key/value pairs here
         :name => "Romeo",
         :age => "15",
         :status => "alive"
-      },
+        },
       :hero_friends => []
-      },
-    :capulet => {
-      #your key/value pairs here!
+   }, 
+   :capulet => {
       :patriarch => {
+        #your key/value pairs here
         :name => "Lord Capulet",
         :age => "50"
-      },
+        },
       :matriarch => {
+        #your key/value pairs here
         :name => "Lady Capulet",
         :age => "51"
-      },
+        },
       :heroine => {
+        #your key/value pairs here
         :name => "Juliet",
         :age => "15",
         :status => "alive"
-      },
+        },
       :heroine_friends => []
-      }
-    }
+   }
+  }
 
-
-end  
-
-
+  
 end
